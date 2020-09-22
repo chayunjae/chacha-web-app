6f487ec2bfa8ce238c0c3c47c981cd698315bc33
chayunjae <ckdbswo93@gmail.com>
2020-09-22 10:02:45 +0900

    Updates

---
+++ precache-manifest.aecc4259d0ed79e50f6d00863e0ff45a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c349a56f413d1c00bc9732600ab4032b",
+    "url": "/chacha-web-app/index.html"
+  },
+  {
+    "revision": "28543a1e3d70492fb7ae",
+    "url": "/chacha-web-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe892a15675f3365b116",
+    "url": "/chacha-web-app/static/js/2.7e6172c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chacha-web-app/static/js/2.7e6172c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28543a1e3d70492fb7ae",
+    "url": "/chacha-web-app/static/js/main.5ad2886d.chunk.js"
+  },
+  {
+    "revision": "d57dbac7524a4948a320",
+    "url": "/chacha-web-app/static/js/runtime-main.169bc69b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chacha-web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
