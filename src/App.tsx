1e3993feae9241db1227f5edc6f072d5cbc2e7e9
chayunjae <ckdbswo93@gmail.com>
2020-09-22 12:13:37 +0900

    준비중

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import coming_soon from "./assets/soon.png";
 import "./App.css";
 
 function App() {
@@ -7,17 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pupple Cake
-        </a>
+        <h2>Coming Soon..</h2>
       </header>
     </div>
   );
